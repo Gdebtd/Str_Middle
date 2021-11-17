f6b7c361f7fbeb117070774bed2c4876677308b4
Gdebtd <76738374+Gdebtd@users.noreply.github.com>
2021-11-17 23:51:49 +0300

    Update functions_3

--- functions_3.cpp
+++ functions_3.cpp
@@ -72,3 +72,29 @@ string itc_decToBase(int num, int base)
 	result = itc_rev_str(result);
 	return result;
 }
+
+void itc_calculate_coordinates()
+{
+	int OX = 0, OY = 0;
+	string str = "";
+	int num = 0;
+	while (str != "000")
+	{
+		cin >> str;
+		if (str != "000")
+			cin >> num;
+		if (str == "North" || str == "South")
+		{
+			if (str == "South")
+				num *= -1;
+			OY += num;
+		}
+		else if (str == "East" || str == "West")
+		{
+			if (str == "West")
+				num *= -1;
+			OX += num;
+		}
+	}
+	cout << OX << " " << OY;
+}
