42a9ddcab09b3c1a984fe8a38ee54016f5d89274
Gdebtd <76738374+Gdebtd@users.noreply.github.com>
2021-11-17 21:08:14 +0300

    Update additional_functions

--- additional_functions.cpp
+++ additional_functions.cpp
@@ -9,3 +9,30 @@ int itc_len(string str)
 	}
 	return i;
 }
+
+int itc_find_str(string str1, string str2)
+{
+	int len2 = itc_len(str2), len = itc_len(str1);
+	int i = 0, i2 = 0, index = -1;
+	while (i < len)
+	{
+		if (str1[i] == str2[i2])
+		{
+			if (str1[i] == str2[0])
+				index = i;
+			i2++;
+		}
+		else
+		{
+			i2 = 0;
+			index = -1;
+		}
+		i++;
+		if (i2 > 0)
+		{
+			if (str2[i2 - 1] == str2[len2 - 1])
+				return index;
+		}
+	}
+	return -1;
+}
